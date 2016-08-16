2e741bad507aae1b5aaa85721c5067cb346e027e
ajnr <navarro11ruiz@gmail.com>
2016-08-16 09:31:24 -0500

    sakdjaskdasdas

---
+++ appEjercicio1/src/main/java/pe/edu/upc/appEjercicio1/App.java
@@ -0,0 +1,31 @@
+package pe.edu.upc.appEjercicio1;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Scanner s =new Scanner(System.in);
+    	int numero;
+    	
+    	double Numero;
+    	//Wrapper (Clases Envolventes) ==> Casting
+    	Double valores=new Double(13);
+    	
+    	String[] arrNombres = new String[3];
+    	
+    	
+        System.out.println( "Hello World!" );
+        
+        for (int i = 0; i < arrNombres.length; i++) {
+			System.out.println( "Nombre"+(i+1));
+			arrNombres[i]=s.nextLine();
+		}
+    }
+ 
+}
